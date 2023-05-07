3ce68bf5b4ed9abf9e1674940e87175db1e4ac01
Aaron Cohen <aaroncohendev@gmail.com>
2023-05-07 23:21:01 +0300

    add mutex

--- linkperf/linkperf.go
+++ linkperf/linkperf.go
@@ -1,5 +1,15 @@
 package linkperf
 
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptrace"
+	"sync"
+	"time"
+
+	"golang.org/x/exp/maps"
+)
+
 // A link performance package that collects data on HTTP performance on a list of links.
 
 //"fmt"
@@ -14,14 +24,14 @@ type Perf struct {
 
 }
 type LinkPerf struct {
-	// mu    sync.Mutex
+	mu    *sync.Mutex
 	perf  map[string][]Perf
 	tries int
 }
 
 // Initialize a new LinkProfiler.
 func NewLinkPerf(links []string, tries int) LinkPerf {
-	lp := LinkPerf{tries: tries, perf: make(map[string][]Perf)}
+	lp := LinkPerf{mu: &sync.Mutex{}, tries: tries, perf: make(map[string][]Perf)}
 	// Transform simple slice of links to internal map of link to
 	// slice of Perf structs. The length of the slice should be initialized
 	// to the amountof tries.
@@ -34,32 +44,40 @@ func NewLinkPerf(links []string, tries int) LinkPerf {
 
 // Runs a performance profile on time to first byte received for
 // links.
-// func (lp *LinkPerf) Run() []Perf {
-// 	ch := make(chan string)
-// 	for _, l := range maps.Keys(lp.perf) {
-// 		go func(l string) {
-// 			req, _ := http.NewRequest("GET", l, nil)
-// 			var start time.Time
-// 			trace := &httptrace.ClientTrace{
-// 				GotFirstResponseByte: func() {
-// 					since := time.Since(start)
-// 					fmt.Printf("Got %s in %v\n", l, since)
-
-// 					//li.perf[l] = append(li.perf[l], int(time.Since(start)/time.Millisecond))
-// 				},
-// 			}
-// 			req = req.WithContext(httptrace.WithClientTrace(req.Context(), trace))
-// 			start = time.Now()
-// 			if _, err := http.DefaultTransport.RoundTrip(req); err != nil {
-// 				li.perf[l] = append(li.perf[l], -1)
-// 				ch <- err.Error()
-// 				return
-// 			}
-// 			ch <- l
-// 		}(l)
-
-// 	}
-// 	for range li.Links {
-// 		<-ch
-// 	}
-// }
+func (lp *LinkPerf) Run() map[string][]Perf {
+	ch := make(chan string)
+	for i, l := range maps.Keys(lp.perf) {
+		for j := 0; j <= lp.tries - 1; j++ {
+			go func(l string, i int) {
+				req, err := http.NewRequest("GET", l, nil)
+				if err != nil {
+					lp.perf[l][i] = Perf{GotFirstResponseByte: -1}
+					ch <- err.Error()
+					return
+				}
+				var start time.Time
+				trace := &httptrace.ClientTrace{
+					GotFirstResponseByte: func() {
+						since := time.Since(start)
+						fmt.Printf("Got %s in %v\n", l, since)
+						lp.mu.Lock()
+						defer lp.mu.Unlock()
+						lp.perf[l][i] = Perf{GotFirstResponseByte: int(time.Since(start) / time.Millisecond)}
+					},
+				}
+				req = req.WithContext(httptrace.WithClientTrace(req.Context(), trace))
+				start = time.Now()
+				if _, err := http.DefaultTransport.RoundTrip(req); err != nil {
+					lp.perf[l][i] = Perf{GotFirstResponseByte: -1}
+					ch <- err.Error()
+					return
+				}
+				ch <- l
+			}(l, i)
+		}
+	}
+	for range maps.Keys(lp.perf) {
+		<-ch
+	}
+	return lp.perf
+}
--- linkperf/linkperf_test.go
+++ linkperf/linkperf_test.go
@@ -5,15 +5,18 @@ import (
 	"testing"
 )
 
-
 func TestNewLinkPerf(t *testing.T) {
 	lp := NewLinkPerf([]string{"https://www.google.com"}, 2)
-	fmt.Println(lp.perf)
 	googlePerf := lp.perf["https://www.google.com"]
 	if len(googlePerf) != 2 {
 		t.Fatalf("wanted googlePerf length to be 2, got %d", len(googlePerf))
 	}
+	
 	if googlePerf[0].GotFirstResponseByte != 0 {
 		t.Fatalf("wanted first perf struct's GotFirstResponseByte to be default value of 0, got: %d", googlePerf[0].GotFirstResponseByte)
 	}
+}
+func TestRun(t *testing.T) {
+	lp := NewLinkPerf([]string{"https://www.google.com"}, 2)
+	fmt.Println(lp.Run())
 }
